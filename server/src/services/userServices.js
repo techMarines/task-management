8d825203187228de30a6d3a9d0147c29c82124dd
Narendra K C <narendrakchoudhary5@gmail.com>
2025-07-28 17:38:50 +0530

    added auth with password hashing and jwt, removed user auth routes from users api routes (#1)

--- server/src/services/userServices.js
+++ server/src/services/userServices.js
@@ -4,21 +4,3 @@ export async function getAllUsers() {
     const users = await prisma.user.findMany();
     return users;
 }
-
-export async function createUser(userName, hashedPassword) {
-    await prisma.user.create({
-        data: {
-            name: userName,
-            hashedPassword: hashedPassword,
-        },
-    });
-}
-
-export async function checkUserExists(userName, hashedPassword) {
-    return await prisma.user.findFirst({
-        where: {
-            name: userName,
-            hashedPassword: hashedPassword,
-        },
-    });
-}
