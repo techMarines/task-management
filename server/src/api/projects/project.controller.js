8c1fe3153040fad4ff6ebe8f6c734f1bb9e74b70
Narendra <narendrakchoudhary5@gmail.com>
2025-08-03 16:56:35 +0530

    added random name generator for default display name

--- server/src/api/projects/project.controller.js
+++ server/src/api/projects/project.controller.js
@@ -3,19 +3,31 @@ import { ApiResponse } from "#utils/api.response";
 import * as projectServices from "#services/project.services";
 
 export async function createProject(req, res) {
+    // userId is put into the req by the authMiddleware
+    const { userId } = req;
     let { projectName, projectDescription } = req.body;
 
     // validate input
     if (!projectName) throw new ApiError(400, "project name can't be empty");
     if (!projectDescription) projectDescription = `${projectName}`;
 
-    const project = await projectServices.createProject(projectName, projectDescription);
+    // this calls a function which uses transaction to create project then create a project role 'manager' and assign it to user
+    const project = await projectServices.createProjectForUser(
+        userId,
+        projectName,
+        projectDescription,
+    );
+    // project is returned if all queries were successfull
     if (!project) throw new ApiError(500, "internal server error, project not created");
 
     res.status(201).json(
         new ApiResponse(
             201,
-            { projectName: project.name, projectDescription: project.description },
+            {
+                projectName: project.name,
+                projectDescription: project.description,
+                starterRole: "Manager",
+            },
             "project created successfuly",
         ),
     );
--- server/src/services/project.services.js
+++ server/src/services/project.services.js
@@ -1,10 +1,33 @@
 import prisma from "#config/prisma.client";
 
-export async function createProject(projectName, projectDescription) {
-    return await prisma.project.create({
-        data: {
-            name: projectName,
-            description: projectDescription,
-        },
+// creates a project, creates a manager role for the project, assigns the manager role to the creater
+export async function createProjectForUser(createrUserId, projectName, projectDescription) {
+    // start an interactive transaction by passing a function to $transaction
+    return await prisma.$transaction(async (tx) => {
+        const project = await tx.project.create({
+            data: {
+                name: projectName,
+                description: projectDescription,
+            },
+        });
+
+        const managerRole = await tx.projectRole.create({
+            data: {
+                name: "Manager",
+                projectId: project.id, // use the id from the project created by above query
+                deletePermission: true,
+                writePermissoin: true,
+            },
+        });
+
+        await tx.userProject.create({
+            data: {
+                userId: createrUserId,
+                projectId: project.id,
+                roleId: managerRole.id, // assign manager role to the creator of the project
+            },
+        });
+
+        return project;
     });
 }
